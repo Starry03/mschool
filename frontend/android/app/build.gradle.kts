d4f0204505b9765e6848915682e4ad9ef7fbb6dc
Starry03 <andrea.starrantino@gmail.com>
2026-07-18 14:42:54 +0200

    refactor: rename project to MSchool and update all package identifiers accordingly

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.school.timetable.frontend"
+    namespace = "com.school.timetable.mschool"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.school.timetable.frontend"
+        applicationId = "com.school.timetable.mschool"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- frontend/android/app/src/main/kotlin/com/school/timetable/frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.school.timetable.frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
